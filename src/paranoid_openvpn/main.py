3ce954a94f4f467bfcb548bd7c71479fd3c2b9c7
Brian Turek <brian.turek@gmail.com>
2021-03-30 06:02:23 +0100

    Refactor how process_profile/process_pia work

--- src/paranoid_openvpn/main.py
+++ src/paranoid_openvpn/main.py
@@ -16,69 +16,91 @@ def process_pia(config: OVPNConfig) -> None:
     """
     cipher_strength = config.cipher_strength()
 
+    if cipher_strength == CipherStrength.STRONG:
+        cipher_settings = [
+            Parameter("cipher", "AES-256-GCM"),
+            Parameter("data-ciphers", "AES-256-GCM:CHACHA20-POLY1305:AES-256-CBC"),
+            Parameter("ncp-disable"),
+        ]
+    else:
+        cipher_settings = [
+            Parameter("cipher", "AES-128-GCM"),
+            Parameter("data-ciphers", "AES-128-GCM:CHACHA20-POLY1305:AES-128-CBC"),
+            Parameter("ncp-disable"),
+        ]
+
+    lines_to_insert = [
+        BlankLine(),
+        Comment("# Begin Paranoid OpenVPN changes"),
+        *cipher_settings,
+        Comment("# End Paranoid OpenVPN changes"),
+        BlankLine(),
+    ]
+
+    # Insert the cipher settings where the old cipher setting was located, also need to clear out the previous settings
     cipher_loc = config.index("cipher")
-    for param in ["cipher", "ncp-disable", "data-ciphers"]:
+    for param in cipher_settings:
         try:
-            del config[param]
+            del config[param.name]
         except KeyError:
             pass
 
-    if cipher_strength == CipherStrength.STRONG:
-        config.insert(cipher_loc, Parameter("ncp-disable"))
-        config.insert(cipher_loc, Parameter("data-ciphers", "AES-256-GCM:CHACHA20-POLY1305:AES-256-CBC"))
-        config.insert(cipher_loc, Parameter("cipher", "AES-256-GCM"))
-    else:
-        config.insert(cipher_loc, Parameter("ncp-disable"))
-        config.insert(cipher_loc, Parameter("data-ciphers", "AES-128-GCM:CHACHA20-POLY1305:AES-128-CBC"))
-        config.insert(cipher_loc, Parameter("cipher", "AES-128-GCM"))
+    for i, line in enumerate(lines_to_insert, start=cipher_loc):
+        config.insert(i, line)
 
 
-def process_profile(src: Path, dest: Path, min_tls: TLSVersion, provider_ext: ProviderExtensions) -> None:
+def process_profile(config: OVPNConfig, min_tls: TLSVersion, provider_ext: ProviderExtensions) -> None:
     """Completely processes one OVPN profile.
 
-    :param src: Path to local input file.
-    :param dest: Path to the desired output file.
+    :param config: OVPN config to modify.
     :param min_tls: Minimum TLS version to require.
     :param provider_ext: Flag to indicate which, if any, provider specific tweaks to apply.
     """
-    config = OVPNConfig.read(src)
     cipher_strength = config.cipher_strength()
 
     if cipher_strength in [CipherStrength.STRONG, CipherStrength.MEDIUM]:
-        security_settings = {
-            "tls-cipher": "TLS-ECDHE-ECDSA-WITH-AES-256-GCM-SHA384:TLS-ECDHE-ECDSA-WITH-CHACHA20-POLY1305-SHA256:TLS-ECDHE-ECDSA-WITH-AES-256-CBC-SHA384",  # noqa: E501
-            "tls-groups": "secp521r1:X448:secp384r1:secp256r1:X25519",
-            "tls-ciphersuites": "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256",
-            "tls-version-min": "{} or-highest".format(min_tls.value),
-        }
+        security_settings = [
+            Parameter("tls-version-min", "{} or-highest".format(min_tls.value)),
+            Parameter(
+                "tls-cipher",
+                "TLS-ECDHE-ECDSA-WITH-AES-256-GCM-SHA384:TLS-ECDHE-ECDSA-WITH-CHACHA20-POLY1305-SHA256:TLS-ECDHE-ECDSA-WITH-AES-256-CBC-SHA384",  # noqa: E501
+            ),
+            Parameter("tls-groups", "secp521r1:X448:secp384r1:secp256r1:X25519"),
+            Parameter("tls-ciphersuites", "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"),
+        ]
     else:
-        security_settings = {
-            "tls-cipher": "TLS-ECDHE-ECDSA-WITH-AES-128-GCM-SHA256:TLS-ECDHE-ECDSA-WITH-CHACHA20-POLY1305-SHA256:TLS-ECDHE-ECDSA-WITH-AES-128-CBC-SHA256",  # noqa: E501
-            "tls-groups": "secp256r1:X25519",
-            "tls-ciphersuites": "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256",
-            "tls-version-min": "{} or-highest".format(min_tls.value),
-        }
+        security_settings = [
+            Parameter("tls-version-min", "{} or-highest".format(min_tls.value)),
+            Parameter(
+                "tls-cipher",
+                "TLS-ECDHE-ECDSA-WITH-AES-128-GCM-SHA256:TLS-ECDHE-ECDSA-WITH-CHACHA20-POLY1305-SHA256:TLS-ECDHE-ECDSA-WITH-AES-128-CBC-SHA256",  # noqa: E501
+            ),
+            Parameter("tls-groups", "secp256r1:X25519"),
+            Parameter("tls-ciphersuites", "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256"),
+        ]
 
-    config.insert(config.last_before_inline(), BlankLine())
-    config.insert(config.last_before_inline(), Comment("# Begin Paranoid OpenVPN changes"))
-    for param, value in security_settings.items():
+    lines_to_insert = [
+        BlankLine(),
+        Comment("# Begin Paranoid OpenVPN changes"),
+        *security_settings,
+        Comment("# End Paranoid OpenVPN changes"),
+        BlankLine(),
+    ]
+
+    for security_setting in security_settings:
         try:
-            del config[param]
+            del config[security_setting.name]
         except KeyError:
             pass
 
-        config.insert(config.last_before_inline(), Parameter(param, value))
-
-    config.insert(config.last_before_inline(), Comment("# End Paranoid OpenVPN changes"))
-    config.insert(config.last_before_inline(), BlankLine())
+    for i, setting in enumerate(lines_to_insert, start=config.last_before_inline()):
+        config.insert(i, setting)
 
     if provider_ext == ProviderExtensions.PIA:
         process_pia(config)
 
-    config.write(dest)
-
     if cipher_strength == CipherStrength.WEAK:
-        warnings.warn(f"{dest} has WEAK cipher strength!")
+        warnings.warn("Profile has WEAK cipher strength!")
 
 
 def process_profiles(src: Path, dest: Path, min_tls: TLSVersion, provider_ext: ProviderExtensions) -> None:
@@ -91,7 +113,9 @@ def process_profiles(src: Path, dest: Path, min_tls: TLSVersion, provider_ext: P
     """
     if src.is_file():
         dest.parent.mkdir(parents=True, exist_ok=True)
-        process_profile(src, dest, min_tls, provider_ext)
+        config = OVPNConfig.read(src)
+        process_profile(config, min_tls, provider_ext)
+        config.write(dest)
     else:
         # If dest is relative to src, rglob will cause this script to infinitely reprocess its own output
         if dest.is_relative_to(src):
@@ -102,6 +126,8 @@ def process_profiles(src: Path, dest: Path, min_tls: TLSVersion, provider_ext: P
             dest_file.parent.mkdir(parents=True, exist_ok=True)
 
             if child.suffix == ".ovpn":
-                process_profile(child, dest_file, min_tls, provider_ext)
+                config = OVPNConfig.read(child)
+                process_profile(config, min_tls, provider_ext)
+                config.write(dest_file)
             else:
                 shutil.copy(child, dest_file)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import py
 import pytest
 from pytest_mock import MockerFixture
@@ -34,76 +32,56 @@ def test_process_pia_not_strong() -> None:
 def test_process_profile_strong(tmpdir: py.path.local) -> None:
     """Test process_profile() for strong ciphers."""
     test_config = OVPNConfig([Parameter("cipher", "aes-256-cbc")])
-    src_path = Path(tmpdir / "test.ovpn")
-    dst_path = Path(tmpdir / "test2.ovpn")
 
-    test_config.write(src_path)
-    process_profile(src_path, dst_path, TLSVersion.v1_3, ProviderExtensions.NONE)
-
-    processed_config = OVPNConfig.read(dst_path)
+    process_profile(test_config, TLSVersion.v1_3, ProviderExtensions.NONE)
 
     assert (
-        processed_config["tls-cipher"].value
+        test_config["tls-cipher"].value
         == "TLS-ECDHE-ECDSA-WITH-AES-256-GCM-SHA384:TLS-ECDHE-ECDSA-WITH-CHACHA20-POLY1305-SHA256:TLS-ECDHE-ECDSA-WITH-AES-256-CBC-SHA384"  # noqa: E501
     )
-    assert processed_config["tls-groups"].value == "secp521r1:X448:secp384r1:secp256r1:X25519"
-    assert processed_config["tls-ciphersuites"].value == "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
-    assert processed_config["tls-version-min"].value == "1.3 or-highest"
+    assert test_config["tls-groups"].value == "secp521r1:X448:secp384r1:secp256r1:X25519"
+    assert test_config["tls-ciphersuites"].value == "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
+    assert test_config["tls-version-min"].value == "1.3 or-highest"
 
 
 def test_process_profile_not_strong(tmpdir: py.path.local) -> None:
     """Test process_profile() for not strong ciphers."""
     test_config = OVPNConfig([Parameter("cipher", "aes-128-cbc")])
-    src_path = Path(tmpdir / "test.ovpn")
-    dst_path = Path(tmpdir / "test2.ovpn")
 
-    test_config.write(src_path)
-    process_profile(src_path, dst_path, TLSVersion.v1_3, ProviderExtensions.NONE)
-
-    processed_config = OVPNConfig.read(dst_path)
+    process_profile(test_config, TLSVersion.v1_3, ProviderExtensions.NONE)
 
     assert (
-        processed_config["tls-cipher"].value
+        test_config["tls-cipher"].value
         == "TLS-ECDHE-ECDSA-WITH-AES-128-GCM-SHA256:TLS-ECDHE-ECDSA-WITH-CHACHA20-POLY1305-SHA256:TLS-ECDHE-ECDSA-WITH-AES-128-CBC-SHA256"  # noqa: E501
     )
-    assert processed_config["tls-groups"].value == "secp256r1:X25519"
-    assert processed_config["tls-ciphersuites"].value == "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256"
-    assert processed_config["tls-version-min"].value == "1.3 or-highest"
+    assert test_config["tls-groups"].value == "secp256r1:X25519"
+    assert test_config["tls-ciphersuites"].value == "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256"
+    assert test_config["tls-version-min"].value == "1.3 or-highest"
 
 
 def test_process_profile_warning_weak(tmpdir: py.path.local) -> None:
     """Test process_profile() to ensure warning is emitted for weak ciphers."""
-    test_config = OVPNConfig([Parameter("cipher", "bf-cbc")])
-    src_path = Path(tmpdir / "test.ovpn")
-    dst_path = Path(tmpdir / "test2.ovpn")
-
-    test_config.write(src_path)
-
     with pytest.warns(UserWarning, match="has WEAK cipher strength!"):
-        process_profile(src_path, dst_path, TLSVersion.v1_3, ProviderExtensions.NONE)
+        test_config = OVPNConfig([Parameter("cipher", "bf-cbc")])
+        process_profile(test_config, TLSVersion.v1_3, ProviderExtensions.NONE)
 
 
 def test_process_profile_pia(mocker: MockerFixture, tmpdir: py.path.local) -> None:
     """Test process_profile() with PIA flag."""
     process_pia_spy = mocker.patch("paranoid_openvpn.main.process_pia", wraps=process_pia)
     test_config = OVPNConfig([Parameter("cipher", "aes-256-cbc")])
-    src_path = Path(tmpdir / "test.ovpn")
-    dst_path = Path(tmpdir / "test2.ovpn")
 
-    test_config.write(src_path)
-
-    process_profile(src_path, dst_path, TLSVersion.v1_3, ProviderExtensions.PIA)
-    processed_config = OVPNConfig.read(dst_path)
+    process_profile(test_config, TLSVersion.v1_3, ProviderExtensions.PIA)
 
     process_pia_spy.assert_called()
 
     assert (
-        processed_config["tls-cipher"].value
+        test_config["tls-cipher"].value
         == "TLS-ECDHE-ECDSA-WITH-AES-256-GCM-SHA384:TLS-ECDHE-ECDSA-WITH-CHACHA20-POLY1305-SHA256:TLS-ECDHE-ECDSA-WITH-AES-256-CBC-SHA384"  # noqa: E501
     )
-    assert processed_config["tls-groups"].value == "secp521r1:X448:secp384r1:secp256r1:X25519"
-    assert processed_config["tls-ciphersuites"].value == "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
-    assert processed_config["tls-version-min"].value == "1.3 or-highest"
-    assert processed_config["cipher"].value == "AES-256-GCM"
-    assert "ncp-disable" in processed_config
-    assert processed_config["data-ciphers"].value == "AES-256-GCM:CHACHA20-POLY1305:AES-256-CBC"
+    assert test_config["tls-groups"].value == "secp521r1:X448:secp384r1:secp256r1:X25519"
+    assert test_config["tls-ciphersuites"].value == "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
+    assert test_config["tls-version-min"].value == "1.3 or-highest"
+    assert test_config["cipher"].value == "AES-256-GCM"
+    assert "ncp-disable" in test_config
+    assert test_config["data-ciphers"].value == "AES-256-GCM:CHACHA20-POLY1305:AES-256-CBC"
